d989e47b1aa104689a24b744804080733a98731c
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-01-21 17:20:21 +0500

    init

---
+++ Multiples/Multiples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Multiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 1;
+            int maxNumber = 28;
+            int beginningRange = 100;
+            int endRange = 1000;
+            int n;
+            int countThreeDigit = 0;
+
+            Random random = new Random();
+
+            n = random.Next(minNumber, maxNumber);
+            Console.WriteLine(n);
+
+            for (int i = n; i <endRange; i+=n)
+            {
+                if (i >= beginningRange)
+                {
+                    Console.WriteLine(i);
+                    countThreeDigit++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Количество трехзначных кратных чисел числа: {n} = {countThreeDigit}");
+        }
+    }
+}
